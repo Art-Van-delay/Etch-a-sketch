f5ebe5a0644ff7ae772fb63513ea0108af2ebbc4
Art <mnsenna@protonmail.com>
2023-02-08 16:45:36 +1000

    Setup grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,15 +3,15 @@ const container = document.querySelector('.container');
 const gridSize = 16;
 
 function createGrid(size) {
-    for (let i = 0; i < 109; i++) {
+    for (let i = 0; i < 16; i++) {
         const div = document.createElement('div');
-        div.className = 'gridItem';
+        div.className = 'grid-item';
         // div.style.border = 'thick solid #0000FF';
         container.appendChild(div);
     }
 }
 
-const divs = document.querySelectorAll('.div');
+const divs = document.querySelectorAll('.grid-item');
 
 divs.forEach((div) => {
     div.addEventListener('mouseover', () => {
